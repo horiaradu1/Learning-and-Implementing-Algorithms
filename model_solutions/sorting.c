ccf76d6a8468236a557893a18a764f18a8947819
Giles Reger <giles.reger@manchester.ac.uk>
2020-11-02 10:21:30 +0000

    Add C model solution

--- model_solutions/sorting.c
+++ model_solutions/sorting.c
@@ -30,14 +30,59 @@ void swap(char* *a, char* *b)
 }
 
 
+
 void insertion_sort(struct darray* arr){
- fprintf(stderr, "Not implemented\n");
- exit(-1);
+        int n = arr->size;
+        int i, j;
+        char* key;
+        // At index i we have sorted the first i elements of the list
+        for (i = 1; i < n; i++)
+        {
+                // Take the next element and insert it into the sorted
+                // part of the list in the right place by shifting
+                // everything below it up until we can insert it
+                key = arr->cells[i];
+                j = i-1;
+                while (j >= 0 && compare(arr->cells[j],key)>0)
+                {
+                        arr->cells[j+1] = arr->cells[j];
+                        j = j-1;
+                }
+                arr->cells[j+1] = key;
+        }
+
 }
 
 
-// Hint: you probably want to define a helper function for the recursive call
+void quick_sort_recursive(struct darray* arr, int start, int end) {
+        if (start >= end)
+                return;
+        // Take the middle element as the pivot 
+        char* mid = arr->cells[end];
+
+        // Hoare partition scheme
+        // Repeatedly shift find the left and rightmost elements that are on
+        // the wrong side and swap them until we meet in the middle
+        int left = start, right = end - 1;
+        while (left < right) {
+                while (compare(arr->cells[left], mid) < 0 && left < right)
+                        left++;
+                while (compare(arr->cells[right],mid) >= 0 && left < right)
+                        right--;
+                swap(&arr->cells[left], &arr->cells[right]);
+        }
+        // Put the pivot in place
+        if (compare(arr->cells[left], arr->cells[end]) >=0)
+                swap(&arr->cells[left], &arr->cells[end]);
+        else
+                left++;
+        // If the pivot was the smallest element then it's already in sorted
+        // position 
+        if (left)
+                quick_sort_recursive(arr, start, left - 1);
+        quick_sort_recursive(arr, left + 1, end);
+}
 void quick_sort(struct darray* arr) {
- fprintf(stderr, "Not implemented\n");
- exit(-1);
+        quick_sort_recursive(arr, 0, arr->size - 1);
 }
+
